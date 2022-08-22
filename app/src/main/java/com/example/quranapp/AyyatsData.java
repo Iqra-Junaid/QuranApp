a85e46c813caae63ece925bb634bf502b772a3c1
Iqra Junaid <bsef19a511@pucit.edu.pk>
2022-08-22 11:59:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quranapp/AyyatsData.java
@@ -0,0 +1,14 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AyyatsData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayyats_data);
+    }
+}
